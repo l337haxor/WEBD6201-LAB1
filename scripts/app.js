37df40fc70a64254c40ffedf07a94f05ab62348e
l337haxor <47485167+l337haxor@users.noreply.github.com>
2020-01-22 14:06:32 -0500

    Added new link and footer

--- scripts/app.js
+++ scripts/app.js
@@ -17,24 +17,42 @@ Since: 2020-01-03
     {
          
         console.log("%c App Started", "font-size: 50px");
-
+        //
         let productsReplace = document.getElementsByClassName("nav-item")[1];
+        //
         productsReplace.lastChild.lastChild.textContent = " Projects";
-        console.log(productsReplace);
+        
+        //Copy the productsReplace object
+        let newLink = productsReplace.cloneNode(true);
+        //Change the icon
+        //newLink.lastChild.setAttribute("class", "fas fa-male");
+        //Change the link name
+        newLink.lastChild.lastChild.textContent = " Human Resources";
+        
+        //Target the last link in the navbar
+        let contactUslink = document.getElementsByClassName("nav-item")[4];
+        //Get a reference to the parent node, ul in this case
+        let parentNode = contactUslink.parentNode;
+        //Insert the new link before the contact us link (last link)
+        parentNode.insertBefore(newLink, contactUslink);
 
-
-
+        /*
         let mainContent = document.getElementById("mainContent");
-
         let h2 = document.createElement("h2");
         h2.setAttribute("class", "display-2");
         h2.textContent = "Content";
-
         mainContent.appendChild(h2);
+        */
 
+        //Get reference to the title section of the page
         let brand = document.getElementsByClassName("navbar-brand")[0];
+        //Change its text to LAB1
+        brand.textContent = "WEBD6201-LAB1";
 
-        brand.textContent = "Client-side Scripting";
+        //Get the foot element
+        let footer = document.getElementById("footer");
+        //Change its text colour to white
+        footer.style.color = "white";
 
     }
     
